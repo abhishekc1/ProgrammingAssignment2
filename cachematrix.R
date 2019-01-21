4f1f5d3ede14f8b7199573198fff3b2749d9b551
abhishekc1 <abhishek.c.oracle@gmail.com>
2019-01-21 22:34:22 +0530

    Code Changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function makeCacheMatrix - It caches 3 Functions - 
+## getInv - to get the Inverse of a matrix using solve Function
+## setInv - to cache the result of the Inverse of the passed Matrix
+## setOrig - to cache the original Matrix for which the Inverse is sought
+## 
+## Function cacheSolve - attempts to get the Inverse of the Matrix, that is passed to it as parameter.
 
-## Write a short comment describing this function
+## This function caches the Original and Inverse Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m_orig <<- matrix()
+  getInv <- function(m){
+    m2 <- solve(m)
+  }
+  setInv <- function(m_inv) m2 <<- m_inv
+  
+  setOrig <- function(m) m_orig <<- m
+  
+  fnc <<- list(getInv = getInv, setInv = setInv, setOrig = setOrig)  
 }
 
 
-## Write a short comment describing this function
+## This function retrieves the Inverse of a Matrix from cache if it is already present or by making a call to the function solve   
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m1, ...) {
+  ## Return a matrix that is the inverse of 'm1'
+  chk <- identical(m1, m_orig)
+  if (chk==FALSE){
+    print("Different Matrix. Get Inverse")
+    #m1_inv <- fnc$getInv(m1)
+	m1_inv <- solve(m1, ...)
+    fnc$setInv(m1_inv)
+    fnc$setOrig(m1)
+    m2
+  }
+  else{
+    print("get cached Data")
+    m2	
+  }
 }
